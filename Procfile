0540a8091a484941024706971e93367a17831ea2
Sally <1218sallychen@gmail.com>
2026-06-22 00:26:50 +0800

    新增部署設定檔

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python pose.py
--- pose.py
+++ pose.py
@@ -227,4 +227,5 @@ def handle_save_record(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port, debug=False)
